3d16480ed5318a24d62f71d1f88923ec1d86d56d
parksaneon <sangeon1500@daum.net>
2021-11-22 19:57:14 +0900

    feat :

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,25 +1,27 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
 
 // action type + creator
-const getTodo = createAction('GET_TODO');
-const addTodo = createAction('ADD_TODO', text);
-const removeTodo = createAction('REMOVE_TODO', id);
-const toggleTodo = createAction('TOGGLE_TODO', id);
+export const getTodo = createAction('GET_TODO');
+export const addTodo = createAction('ADD_TODO');
+export const removeTodo = createAction('REMOVE_TODO');
+export const toggleTodo = createAction('TOGGLE_TODO');
 
 const initial = [{ id: Date.now(), value: '리스트 추가하기', complete: false }];
 
 // reducer
-const todos = createReducer(state=initial, {
+export const todos = createReducer(initial, {
   [getTodo]: (state) => state,
-  [addTodo]: (state, action) => [...state, {
-    id: Date.now(),
-    value: action.text,
-    complete: false,
-  },],
+  [addTodo]: (state, action) => [
+    ...state,
+    {
+      id: Date.now(),
+      value: action.text,
+      complete: false,
+    },
+  ],
   [removeTodo]: (state, action) => state.filter((todo) => todo.id !== action.id),
-  [toggleTodo]: (state, action) => state.forEach((todo) => {
-    if(todo.id === action.id) todo.complete = !todo.complete;
-  }),
+  [toggleTodo]: (state, action) =>
+    state.forEach((todo) => {
+      if (todo.id === action.id) todo.complete = !todo.complete;
+    }),
 });
-
-export todos;
